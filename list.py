65008978be9305330c0cd4b16d15cb489ba9a705
Alba-ez <alba.herraez@gmail.com>
2024-06-24 17:50:48 +0200

    Adding in variables.js

---
+++ list.py
@@ -0,0 +1,16 @@
+##IN: [1,4,3,1,2,1,3] OUT: [1]
+
+def mostFrecuent(arr,n) :
+    maxcount = 0 ;
+    element_having_max_freq = 0 ;
+
+    for i in range(0,n) :
+        count = 0
+        for j in range(0,n) :
+            if (arr[i] == arr[j]) :
+                count += 1
+        if (count > maxcount) :
+            maxcount = count
+            element_having_max_freq = arr[i]
+
+    return element_having_max_freq ;
