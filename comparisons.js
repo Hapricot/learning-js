4ecabee5f3f23cb3033b0de85c604db20d768c77
Alba-ez <alba.herraez@gmail.com>
2024-06-25 11:56:22 +0200

    Adding in comparison.js

---
+++ comparisons.js
@@ -0,0 +1,17 @@
+console.log("Does 5 equal 4? " + (5 == 4))
+console.log("Does 5 equal 5? " + (5 == 5))
+console.log("Does 4 not equal 5? " + (4 != 5))
+
+console.log("Is 5 greater than 4? " + (5 > 4))
+console.log("Is 4 less than 5? " + (5 < 4))
+console.log("Is 4 les than or equal to 5? " + (4 <= 5))
+console.log("Is 5 greater than or equal to 5? " + (5 >= 5))
+
+let x = 3
+let y = 2
+
+console.log("Is x greater than y? " + (x > y))
+console.log("Is x equal to y? " + (x == y))
+
+//Don't do this! This will assign the variable value, not compare the values
+console.log(x = y)
